3913f9d377fac9c0220dadea2ea6771726f2d7cd
Marco Goedert <marco.goedert@edu.pucrs.br>
2020-09-14 20:01:07 -0300

    Add support to TCP file transfering

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,32 +1,57 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.InetAddress;
+import java.io.InputStream;
+import java.io.File;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TCPClient {
-    private Socket socket;
-    private Scanner scanner;
-    private TCPClient(InetAddress serverAddress, int serverPort) throws Exception {
-        this.socket = new Socket(serverAddress, serverPort);
-        this.scanner = new Scanner(System.in);
-    }
-    private void start() throws IOException {
-        String input;
-        while (true) {
-            input = scanner.nextLine();
-            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-            out.println(input);
-            out.flush();
-        }
-    }
-    
-    public static void main(String[] args) throws Exception {
-        TCPClient client = new TCPClient(
-                InetAddress.getByName(args[0]), 
-                Integer.parseInt(args[1]));
+    public static void main (String [] args ) throws IOException {
         
-        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
-        client.start();                
+        final String fileName = "received_file.txt";
+        final String srcDir = System.getProperty("user.dir");
+
+        // monta path dos arquivos a serem enviados
+        String absoluteFilePath = srcDir + File.separator + "receive" + File.separator + fileName;
+
+        final String SERVER = "127.0.0.1";  // localhost
+        final String FILE_TO_RECEIVED = absoluteFilePath;  
+
+        final int FILE_SIZE = 10*1024;  
+        
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket sock = null;
+        try
+        {
+            sock = new Socket(SERVER, 9876);
+            System.out.println("Connecting...");
+
+            // receive file
+            byte [] mybytearray  = new byte [FILE_SIZE];
+            InputStream is = sock.getInputStream();
+            fos = new FileOutputStream(FILE_TO_RECEIVED);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = is.read(mybytearray,0,mybytearray.length);
+            current = bytesRead;
+
+            do {
+                bytesRead =
+                    is.read(mybytearray, current, (mybytearray.length-current));
+                if(bytesRead >= 0) current += bytesRead;
+            } while(bytesRead > -1);
+
+            bos.write(mybytearray, 0 , current);
+            bos.flush();
+            System.out.println("File " + FILE_TO_RECEIVED
+                + " downloaded (" + current + " bytes read)");
+        }
+        finally {
+            if (fos != null) fos.close();
+            if (bos != null) bos.close();
+            if (sock != null) sock.close();
+        }
     }
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,42 +1,61 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-        
+
 public class TCPServer {
-    private ServerSocket server;
-    public TCPServer(String ipAddress) throws Exception {
-        if (ipAddress != null && !ipAddress.isEmpty()) 
-          this.server = new ServerSocket(0, 1, InetAddress.getByName(ipAddress));
-        else 
-          this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
-    }
-    private void listen() throws Exception {
-        String data = null;
-        Socket client = this.server.accept();
-        String clientAddress = client.getInetAddress().getHostAddress();
-        System.out.println("\r\nNew connection from " + clientAddress);
-        
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(client.getInputStream()));        
-        while ( (data = in.readLine()) != null ) {
-            System.out.println("\r\nMessage from " + clientAddress + ": " + data);
-        }
-    }
-    public InetAddress getSocketAddress() {
-        return this.server.getInetAddress();
-    }
+
+  public static void main (String [] args ) throws IOException {
+    // guarda nome dos arquivos para montar os paths
+    //final String tinyFileName = "file_less_then_1500bytes.txt";
+    final String bigFileName = "file_more_then_10000bytes.txt";
+    final String srcDir = System.getProperty("user.dir");
+
+    // monta path dos arquivos a serem enviados
+    //String absoluteTinyFilePath = srcDir + File.separator + "send" + File.separator + tinyFileName;
+    String absoluteBigFilePath = srcDir + File.separator + "send" + File.separator + bigFileName;
+    //String filePath = null;
+
+    final int SOCKET_PORT = 9876;
+    final String FILE_TO_SEND = absoluteBigFilePath;
+
+    FileInputStream fis = null;
+    BufferedInputStream bis = null;
+    OutputStream os = null;
+    ServerSocket servsock = null;
+    Socket sock = null;
     
-    public int getPort() {
-        return this.server.getLocalPort();
+    try {
+      servsock = new ServerSocket(SOCKET_PORT);
+      while (true) {
+        System.out.println("Waiting...");
+        try {
+          sock = servsock.accept();
+          System.out.println("Accepted connection : " + sock);
+          // send file
+          File myFile = new File (FILE_TO_SEND);
+          byte [] mybytearray  = new byte [(int)myFile.length()];
+          fis = new FileInputStream(myFile);
+          bis = new BufferedInputStream(fis);
+          bis.read(mybytearray,0,mybytearray.length);
+          os = sock.getOutputStream();
+          System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+          os.write(mybytearray,0,mybytearray.length);
+          os.flush();
+          System.out.println("Done.");
+        }
+        finally {
+          if (bis != null) bis.close();
+          if (os != null) os.close();
+          if (sock!=null) sock.close();
+        }
+      }
     }
-    public static void main(String[] args) throws Exception {
-        TCPServer app = new TCPServer(args[0]);
-        System.out.println("\r\nRunning Server: " + 
-                "Host=" + app.getSocketAddress().getHostAddress() + 
-                " Port=" + app.getPort());
-        
-        app.listen();
+    finally {
+      if (servsock != null) servsock.close();
     }
+  }
 }
