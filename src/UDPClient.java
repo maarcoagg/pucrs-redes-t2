19942c1ed4428e72578886129431448d5d8f0daf
Marco Goedert <marco.goedert@edu.pucrs.br>
2020-09-14 19:27:59 -0300

    Add support to UDP file transfering

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -3,12 +3,50 @@
 
 import java.io.*; // classes para input e output streams e
 import java.net.*;// DatagramaSocket,InetAddress,DatagramaPacket
+import java.util.Scanner;
 
 class UDPClient {
    public static void main(String args[]) throws Exception
    {
-      // cria o stream do teclado
-      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+      // guarda nome dos arquivos para montar os paths
+      final String tinyFileName = "file_less_then_1500bytes.txt";
+      final String bigFileName = "file_more_then_10000bytes.txt";
+      final String srcDir = System.getProperty("user.dir");
+
+      // monta path dos arquivos a serem enviados
+      String absoluteTinyFilePath = srcDir + File.separator + "send" + File.separator + tinyFileName;
+      String absoluteBigFilePath = srcDir + File.separator + "send" + File.separator + bigFileName;
+      String filePath = null;
+
+      // pergunta ao cliente qual arquivo deseja enviar
+      System.out.print("\t1 - " + tinyFileName + "\n" +
+                        "\t2 - " + bigFileName + "\n"+
+                        "Selecione qual arquivo enviar (1-2): ");
+      Scanner input = new Scanner(System.in);
+      Integer option = input.nextInt();
+
+      if (option.equals(1))
+         filePath = absoluteTinyFilePath;
+      else if (option.equals(2))
+         filePath = absoluteBigFilePath;
+      else
+      {
+         System.err.println("Opcao invalida! Abortando execucao...");
+         System.exit(1);
+      }
+
+      File selectedFile = new File(filePath);
+      FileInputStream fis = new FileInputStream(selectedFile);
+      int fileSize = fis.available();
+      System.err.println("File size:"+fileSize);
+      byte[] sendData = new byte[fileSize];
+      //byte[] receiveData = new byte[1024];
+
+      //contador de bytes
+      for (int i = 0; i < fileSize; i++)
+      {
+         sendData[i] = (byte) fis.read();
+      }
 
       // declara socket cliente
       DatagramSocket clientSocket = new DatagramSocket();
@@ -16,20 +54,17 @@ class UDPClient {
       // obtem endereco IP do servidor com o DNS
       InetAddress IPAddress = InetAddress.getByName("localhost");
 
-      byte[] sendData = new byte[1024];
-      //byte[] receiveData = new byte[1024];
-
-      // le uma linha do teclado
-      String sentence = inFromUser.readLine();
-      sendData = sentence.getBytes();
-
       // cria pacote com o dado, o endereco do server e porta do servidor
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 
       //envia o pacote
       clientSocket.send(sendPacket);
 
+      System.out.println("Arquivo enviado com sucesso.");
+
       // fecha o cliente
       clientSocket.close();
+      fis.close();
+      input.close();
    }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -2,37 +2,55 @@
 // Separa o dado, o endereco IP e a porta deste cliente
 // Imprime o dado na tela
 
-//import java.io.*;
+import java.io.*;
 import java.net.*;
+
 
 class UDPServer {
    public static void main(String args[])  throws Exception
       {
+         // guarda nome dos arquivos para montar os paths
+         final String fileName = "received_file.txt";
+         final String srcDir = System.getProperty("user.dir");
+
+         // monta path dos arquivos a serem enviados
+         String absoluteFilePath = srcDir + File.separator + "receive" + File.separator + fileName;
+
          // cria socket do servidor com a porta 9876
          DatagramSocket serverSocket = new DatagramSocket(9876);
 
-         byte[] receiveData = new byte[1024];
+         byte[] receiveData = new byte[10*1024];
+
+         FileOutputStream fos = new FileOutputStream(absoluteFilePath);
+         
+         // declara o pacote a ser recebido
+         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
          while(true)
          {
+            System.out.println("Aguardando...");
+
             // declara o pacote a ser recebido
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            //DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
             // recebe o pacote do cliente
             serverSocket.receive(receivePacket);
 
             // pega os dados, o endereco IP e a porta do cliente
             // para poder mandar a msg de volta
-            String sentence = new String(receivePacket.getData());
+            String fileData = new String(receivePacket.getData());
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
 
-            System.out.println("Mensagem recebida: " + sentence);
-            System.out.println("IP Address: " + IPAddress);
-            System.out.println("Port: " + port);
+            fos.write(receiveData);
 
-            if (sentence.equals("sair"))
+            System.out.println("Mensagem recebida: " + fileData);
+            System.out.println("IP Address: " + IPAddress + "\tPort: "+port);
+
+            if (fileData.equals(null))
                break;
          }
          serverSocket.close();
+         fos.close();
       }
 }
